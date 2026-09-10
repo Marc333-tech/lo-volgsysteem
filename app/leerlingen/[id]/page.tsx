965c31794ee989b85a117d5ca74cebb4777593c4
Boiten <m.boiten@rsgterapel.nl>
2026-09-10 12:52:11 +0200

    Softbal beoordelingen opslaan in Supabase

--- app/leerlingen/[id]/page.tsx
+++ app/leerlingen/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { supabase } from "../../../lib/supabase";
 
 export default async function LeerlingPage({
@@ -36,6 +37,13 @@ export default async function LeerlingPage({
           <p>
             <strong>Klas:</strong> {leerling.klas}
           </p>
+
+          <Link
+            href={`/leerlingen/${id}/softbal`}
+            className="inline-block mt-6 rounded-lg bg-slate-800 px-4 py-2 text-white hover:bg-slate-700"
+          >
+            Softbal beoordelen
+          </Link>
         </div>
       </div>
     </main>
